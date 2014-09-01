b5418b13a68d45bafbaab4c3ceb9e5cc8fef9434
Eduardo <edubaik@gmail.com>
2014-09-01 02:02:36 -0700

    Changed title to Kira

--- application/views/posts/new.php
+++ application/views/posts/new.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>Welcome to Diary</title>
+        <title>Welcome to Kira</title>
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Latest compiled and minified CSS -->
