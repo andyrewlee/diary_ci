618d02fface5387889941b3d393563381ba45a34
Eduardo Baik <eduardobaik@eduardos-air.local.tld>
2014-08-31 23:16:32 -0700

    Created navbar

--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -17,11 +17,11 @@
         <link rel="stylesheet" type="text/css" href="../../assets/css/style.css">
     </head>
     <body>
-        <nav class="navbar navbar-default" role="navigation">
-            <div class="container-fluid">
-              <!-- Brand and toggle get grouped for better mobile display -->
+        
+        <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="container">
                 <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
                         <span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
@@ -29,16 +29,15 @@
                     </button>
                     <a class="navbar-brand" href="#">Diary</a>
                 </div>
-
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <div class="collapse navbar-collapse">
                     <ul class="nav navbar-nav navbar-right">
                         <li><a href="#">About</a></li>
-                        <li><a href="#">Sign up</a></li>
-                        <li><a href="#">Log in</a></li>
+                        <li><a href="#register">Register</a></li>
+                        <li><a href="#login">Login</a></li>
                     </ul>
-                </div><!-- /.navbar-collapse -->
-            </div><!-- /.container-fluid -->
-        </nav>
+                </div><!--/.nav-collapse -->
+            </div>
+        </div>
+        
     </body>
 </html>
